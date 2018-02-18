53121f0db31c6e74495c2211d8227764b72e74e7
Ahalya Mandana <ahalyamandana@gmail.com>
2018-02-18 16:29:00 -0600

    Adding JS file

---
+++ js/index.js
@@ -0,0 +1,27 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    console.log('as');
+    document.getElementById("myDropdown").classList.toggle("show");
+    console.log('as');
+}
+
+console.log('asdf');
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function setInput(id) {
+  document.getElementById("question1Input").value = id
+}
