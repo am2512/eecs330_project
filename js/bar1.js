7bd3303af9a0c50787fa0b4cf08463214e03a514
danimal4580 <danielknight2019@u.northwestern.edu>
2018-03-14 12:41:51 -0500

    Update bar1.js

--- js/bar1.js
+++ js/bar1.js
@@ -1,12 +1,20 @@
+
+
+var globalLabels = ["Playing Piano", "Meditation", "Homework", "Working Out"]
+var globalBackgroundColors = ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"]
+var globalData = [.5,2,6,5]
+
 new Chart(document.getElementById("bar-chart"), {
+
     type: 'bar',
     data: {
       labels: ["Playing Piano", "Meditation", "Homework", "Working Out"],
+      // labels: ["Playing Piano", "Meditation", "Homework", "Working Out"],
       datasets: [
         {
           label: "Stress levels",
           backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
-          data: [0.5,5,7,6]
+          data: [10,5,7,6]
         }
       ]
     },
@@ -15,32 +23,96 @@ new Chart(document.getElementById("bar-chart"), {
       title: {
         display: true,
         text: 'Time spent on each activity'
-      }
+      },
+
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+                // suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                // OR //
+                beginAtZero: true   // minimum value will be 0.
+            }
+        }]
+       }
+
     }
 });
 
-var p = document.getElementById("Twoslider");
+var p = document.getElementById("button-0-1");
 p.addEventListener("input", function() {
 
-  new Chart(document.getElementById("bar-chart"), {
+
+localLabels = []
+localBackgroundColors = []
+localData = []
+binaryString = document.getElementById("button-0-1").dataset.binaryString
+console.log(binaryString)
+var i = 0
+for (i; i<binaryString.length ; i++ ) {
+  if ('1' == binaryString.charAt(i)) {
+    localLabels.push(globalLabels[i])
+    localBackgroundColors.push(globalBackgroundColors[i])
+    localData.push(globalData[i])
+  }
+}
+console.log(localLabels)
+console.log(localBackgroundColors)
+console.log(localData)
+
+new Chart(document.getElementById("bar-chart"), {
       type: 'bar',
       data: {
-        labels: ["Playing Piano", "Meditation", "Homework", "Working Out"],
+        labels: localLabels,
         datasets: [
           {
             label: "Stress levels",
-            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-            data: [3,0.5,6,5]
+            backgroundColor: localBackgroundColors,
+            data: localData
           }
         ]
       },
       options: {
+        // scaleOverride : true,
         legend: { display: false },
         title: {
           display: true,
           text: 'Time spent on each activity'
-        }
-      }
+        },
+        scaleBeginAtZero: true,
+        // asdf
+        scaleStartValue: 0,
+    //     yAxes: [{
+    //     ticks: {
+    //         min: 0,
+    //         max: 100,
+    //         stepSize: 20
+    //     }
+    // }]
+
+
+
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+                // suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                // OR //
+                beginAtZero: true   // minimum value will be 0.
+            }
+        }]
+       }
+
+
+        // scales: {
+        //   yAxes: [{
+        //     ticks: {
+        //       beginAtZero: true,
+        //       min: 0
+        //     }
+        //   }]
+        // }
+    }
   });
 
 
