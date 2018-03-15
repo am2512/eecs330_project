8b0004122e3eaee77e35c324441e42ba956c932d
danimal4580 <danielknight2019@u.northwestern.edu>
2018-03-15 15:57:55 -0500

    Update line1.js

--- js/line1.js
+++ js/line1.js
@@ -1,4 +1,5 @@
-new Chart(document.getElementById("line-chart"), {
+
+myLineChart = new Chart(document.getElementById("line-chart"), {
   type: 'line',
   data: {
     labels: [1,2,3,4,5,6,7,8,9,10],
