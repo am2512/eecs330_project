4641aaaa56e79827bedff39d0fa5e3f4fe1bd57a
danimal4580 <danielknight2019@u.northwestern.edu>
2018-03-15 14:55:10 -0500

    Update login.js

--- login.js
+++ login.js
@@ -11,7 +11,7 @@
        alertString = alertString + 'You have not entered a username.\n'
      }
      if (pWord.length === 0) {
-        alertString = alertString + 'You have not entered password.\n'
+        alertString = alertString + 'You have not entered a password.\n'
      }
      if (alertString.length === 0) {
         console.log('there4')
@@ -25,5 +25,38 @@
         return 1
      }
          
-  
+ }
+
+
+ function isEmptyCreate()(){ 
+    console.log('here')
+     var alertString = ''
+     var uName =  document.getElementById("username").value
+     var pWord = document.getElementById("password").value
+     var pWord2 = document.getElementById("passwordtwo").value
+     
+     if (uName.length === 0) {
+       alertString = alertString + 'You have not entered a username.\n'
+     }
+     if (pWord.length === 0) {
+        alertString = alertString + 'You have not entered a password.\n'
+     }
+     if (pWord.length === 0) {
+        alertString = alertString + 'You have not reentered your password.\n'
+     }
+     if (pWord != pWord2) {
+        alertString = alertString + 'Passwords do not matched.\n'
+     }
+     if (alertString.length === 0) {
+        console.log('there4')
+        window.event.returnValue = false;
+        document.location.href = "index.html";
+        console.log('there1')
+        return 0 
+     } else {
+        alert(alertString + "Please check your inputs.")
+        console.log('therasdfe')
+        return 1
+     }
+         
  }
