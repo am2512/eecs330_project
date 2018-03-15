174bd08da7b0af1c22477fa15bb6aaef6a94fb4f
danimal4580 <danielknight2019@u.northwestern.edu>
2018-03-15 16:03:10 -0500

    Update part2.js

--- js/part2.js
+++ js/part2.js
@@ -4,7 +4,40 @@ toggle between hiding and showing the dropdown content */
 // data-old-value="300" 
 // document.getElementById("Twoslider").dataset.binaryString = '1111'
 myBarChart = null
-myLineChart = null
+myLineChart = new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+    labels: [1,2,3,4,5,6,7,8,9,10],
+    datasets: [{ 
+        data: [1,1,1,1,2,6,7,8,7,1],
+        label: "Playing Piano",
+        borderColor: "#3e95cd",
+        fill: false
+      }, { 
+        data: [3,3,4,8,9,2,1,4,6,1],
+        label: "Meditation",
+        borderColor: "#8e5ea2",
+        fill: false
+      }, { 
+        data: [1,3,3,9,9,7,6,1,1,1],
+        label: "Homework",
+        borderColor: "#3cba9f",
+        fill: false
+      }, { 
+        data: [4,5,2,7,8,1,1,1,1,1],
+        label: "Working Out",
+        borderColor: "#e8c3b9",
+        fill: false
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Stress levels for each activity'
+    }
+  }
+});
 document.getElementById("button-0-1").dataset.binaryString = '1111'
 var lineData = [
   [1,1,1,1,2,6,7,8,7,1],
