28e13de4c27f5bbc905890c77c5e571552f22112
danimal4580 <danielknight2019@u.northwestern.edu>
2018-03-15 14:29:31 -0500

    Update login.js

--- login.js
+++ login.js
@@ -2,20 +2,24 @@
 
 
  function isEmpty(){ 
+    console.log('here')
      var alertString = ''
      var uName =  document.getElementById("username").value
      var pWord = document.getElementById("password").value
+     
      if (uName.length === 0) {
        alertString = alertString + 'You have not entered a username.\n'
      }
      if (pWord.length === 0) {
         alertString = alertString + 'You have not entered password.\n'
      }
-     if (alertString.length === 0)
+     if (alertString.length === 0) {
+        console.log('there')
         return 0 
-     }else {
-      alert(alertString + 'Please check your inputs."
-            return 1
+     } else {
+        alert(alertString + 'Please check your inputs.")
+        console.log('therasdfe')
+        return 1
      }
          
   
