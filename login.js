eb7f756ef75d95595ddb601fbe1e2b881223b96f
danimal4580 <danielknight2019@u.northwestern.edu>
2018-03-16 09:01:43 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,9 @@
-
-
-
  function isEmpty(){ 
     console.log('here')
      var alertString = ''
      var uName =  document.getElementById("username").value
      var pWord = document.getElementById("password").value
+//      var pWord2 = document.getElementById("passwordtwo").value
      
      if (uName.length === 0) {
        alertString = alertString + 'You have not entered a username.\n'
@@ -13,9 +11,16 @@
      if (pWord.length === 0) {
         alertString = alertString + 'You have not entered a password.\n'
      }
+//      if (pWord.length === 0) {
+//         alertString = alertString + 'You have not reentered your password.\n'
+//      }
+//      if (pWord != pWord2) {
+//         alertString = alertString + 'Passwords do not match.\n'
+//      }
      if (alertString.length === 0) {
-        console.log('theradfe4')
+        console.log('thsadffdsere4')
 //         window.event.returnValue = false;
+//         alert("Rerouting. Account succsefully made. Click to Continue.")
         document.location.href = "createaccount.html";
         console.log('there1')
         return 0 
@@ -24,8 +29,33 @@
         console.log('therasdfe')
         return 1
      }
-         
  }
+
+//  function isEmpty(){ 
+//     console.log('here')
+//      var alertString = ''
+//      var uName =  document.getElementById("username").value
+//      var pWord = document.getElementById("password").value
+     
+//      if (uName.length === 0) {
+//        alertString = alertString + 'You have not entered a username.\n'
+//      }
+//      if (pWord.length === 0) {
+//         alertString = alertString + 'You have not entered a password.\n'
+//      }
+//      if (alertString.length === 0) {
+//         console.log('theradfe4')
+// //         window.event.returnValue = false;
+//         document.location.href = "createaccount.html";
+//         console.log('there1')
+//         return 0 
+//      } else {
+//         alert(alertString + "Please check your inputs.")
+//         console.log('therasdfe')
+//         return 1
+//      }
+         
+//  }
 
 
  function isEmptyCreate(){ 
