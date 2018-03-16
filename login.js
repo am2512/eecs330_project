906c623b6b5004da283562344d1ccc3748512454
danimal4580 <danielknight2019@u.northwestern.edu>
2018-03-16 09:04:50 -0500

    Update login.js

--- login.js
+++ login.js
@@ -2,35 +2,35 @@
     console.log('here')
   document.location.href = "createaccount.html";
   return 0;
-     var alertString = ''
-     var uName =  document.getElementById("username").value
-     var pWord = document.getElementById("password").value
-//      var pWord2 = document.getElementById("passwordtwo").value
+//      var alertString = ''
+//      var uName =  document.getElementById("username").value
+//      var pWord = document.getElementById("password").value
+// //      var pWord2 = document.getElementById("passwordtwo").value
      
-     if (uName.length === 0) {
-       alertString = alertString + 'You have not entered a username.\n'
-     }
-     if (pWord.length === 0) {
-        alertString = alertString + 'You have not entered a password.\n'
-     }
+//      if (uName.length === 0) {
+//        alertString = alertString + 'You have not entered a username.\n'
+//      }
 //      if (pWord.length === 0) {
-//         alertString = alertString + 'You have not reentered your password.\n'
+//         alertString = alertString + 'You have not entered a password.\n'
 //      }
-//      if (pWord != pWord2) {
-//         alertString = alertString + 'Passwords do not match.\n'
+// //      if (pWord.length === 0) {
+// //         alertString = alertString + 'You have not reentered your password.\n'
+// //      }
+// //      if (pWord != pWord2) {
+// //         alertString = alertString + 'Passwords do not match.\n'
+// //      }
+//      if (alertString.length === 0) {
+//         console.log('thsadffdsere4')
+// //         window.event.returnValue = false;
+// //         alert("Rerouting. Account succsefully made. Click to Continue.")
+//         document.location.href = "createaccount.html";
+//         console.log('there1')
+//         return 0 
+//      } else {
+//         alert(alertString + "Please check your inputs.")
+//         console.log('therasdfe')
+//         return 1
 //      }
-     if (alertString.length === 0) {
-        console.log('thsadffdsere4')
-//         window.event.returnValue = false;
-//         alert("Rerouting. Account succsefully made. Click to Continue.")
-        document.location.href = "createaccount.html";
-        console.log('there1')
-        return 0 
-     } else {
-        alert(alertString + "Please check your inputs.")
-        console.log('therasdfe')
-        return 1
-     }
  }
 
 //  function isEmpty(){ 
